2b3a55eca9d07f2fb8453f3a5d17cc3add78e826
Johnny Chan <129523730+Changtze@users.noreply.github.com>
2026-09-14 22:39:11 +0100

    Update NTFY_TOPIC and clean up imports

--- booking_monitor.py
+++ booking_monitor.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 
 # Configuration
 URL = "https://booking.bookinghound.cloud/fe/booking?og=bdffd003-ab61-4b27-8513-d0e8ef0f1425&mode=sl"
-NTFY_TOPIC = "trapeze" # CHANGE THIS to a unique, random string
+NTFY_TOPIC = "gorilla_trapeze" # CHANGE THIS to a unique, random string
 CHECK_INTERVAL_SECONDS = 300 # Checks every 5 minutes
 
 def send_push_notification(message):
